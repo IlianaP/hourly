2bb4586c56dc05e8dbd9115f863509e5107e0af5
IPaleva <ip97@cornell.edu>
2020-10-16 04:04:54 +0000

    Adding a create function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,15 @@ class ProjectsController < ApplicationController
 	def new 
 		@project = Project.new
 	end
+
+	def create
+		Project.create(project_params)
+		redirect_to root_path
+    end
+
+    private
+    def project_params
+    params.require(:project).permit(:name, :description, :plan, :planhours, :thours)
+    end
+
 end
